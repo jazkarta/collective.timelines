1f68d89d690c3c7a0c8d811e247635f7a966514a
Alec Mitchell <alecpm@gmail.com>
2012-11-16 17:36:49 -0800

    Use the proper extension for the readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.timelines',
       version=version,
       description="Timeline views for collections and folders (using verite TimelineJS).",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
