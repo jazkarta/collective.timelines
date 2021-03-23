276d07c183149ec07a69274ad2309a80e8c082da
Alec Mitchell <alecpm@gmail.com>
2021-03-23 10:50:58 -0700

    Remove PasteScript dependency.

--- setup.py
+++ setup.py
@@ -35,6 +35,4 @@ setup(name='collective.timelines',
       entry_points="""[z3c.autoinclude.plugin]
 target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
